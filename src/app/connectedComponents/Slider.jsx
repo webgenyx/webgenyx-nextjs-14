25fb736174d9a102840c81b540d414ec31d311d5
akilinjamam <akilinjamam@gmail.comm>
2024-02-26 08:41:29 +0600

    file-updated

--- src/app/connectedComponents/Slider.jsx
+++ src/app/connectedComponents/Slider.jsx
@@ -84,38 +84,3 @@ const Slider = ({data}) => {
 };
 
 export default Slider;
-
-
-/* 
-
-<div className={servicePage.container}>
-        <Image onClick={() => setCount(count === 1 ? () => setCount(data?.length) : count - 1)} style={{backgroundColor:'rgba(255, 255, 255, 1)', padding:'3px', borderRadius:'3px', cursor:'pointer'}} src={leftAng} alt=''/>
-        <div className={servicePage.imgBox}>
-        {
-                    copiedData?.map(d =>
-                        <div key={d.id}
-                            style={{
-                                transition: `transform ${count > 0 ? 1 : 0}s`,
-                                transform: `translateX(${count * -100}%)`,
-                            }}
-
-                        >
-                            <div
-                                style={{ width: '100vw', animationDuration: '16s', }}
-                                onMouseEnter={() => setPause(true)}
-                                onMouseLeave={() => setPause(false)}
-                            >
-                                
-                                <div  style={{ width: '950px' }} >
-                                <Image style={{display:'block', margin:'auto'}} src={d?.bannerImg} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-        </div>
-      
-        <Image onClick={() => setCount(count === data?.length ? () => setCount(0) : count + 1)} style={{backgroundColor:'rgba(255, 255, 255, 1)', padding:'3px', borderRadius:'3px', cursor:'pointer'}} src={rightAng} alt=''/>
-        </div>
-
-*/
